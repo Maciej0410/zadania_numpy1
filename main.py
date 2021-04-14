40d192f6e904129f68839abb407d6a391a0537c1
Maciej Warych <macmrs@o2.pl>
2021-04-14 18:03:50 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+# zad1
+
+# tablica= np.arange(4,84,4)
+
+# print(tablica)
+
+# =============================
+
+# zad2
+
+# tablica2= np.array([2.25,5.05,4.43,7.89,6.45,8.75,0.87])
+
+# z= np.fromiter(tablica2, dtype='int32')
+
+# print(tablica2)
+# print(z)
+
+# ================================
+
+# zad3
+
+# def macierz(n : int):
+#     a = 2**np.mgrid[1:n+1, 1:n+1]
+#     return a
+#
+# print(macierz(7))
+
+#===============================================
+
+#zad4
+
+# def tab_potegowanie(n, m):
+#     a = np.logspace(start=1,num=m, stop=m, base=n, dtype='int64')
+#     return a
+#
+# print(tab_potegowanie(3, 5))
+
+#zad5
